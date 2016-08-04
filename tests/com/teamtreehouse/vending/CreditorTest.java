3a0f3b5ffbb3198f849979c003d583866f1cf185
Charles-A. Francisco <charlantfr@gmail.com>
2016-08-03 20:17:30 -0400

    fork - first commit

---
+++ tests/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,13 @@
+package com.teamtreehouse.vending;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreditorTest {
+  @Test
+  public void refund() throws Exception {
+
+  }
+
+}
